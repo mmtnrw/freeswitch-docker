462859ddc443a3956fac260eaf7876e9fefc4d5e
mmtnrw <46136708+mmtnrw@users.noreply.github.com>
2020-01-10 11:09:37 +0100

    Update start.sh

--- root/start.sh
+++ root/start.sh
@@ -1,6 +1,33 @@
 #!/bin/sh
 
-echo "Updating Clock"
-hwclock -s
+echo "[info] Setting up User ID: ${PUID}"
+echo "[info] Setting up Group ID: ${PGID}"
+echo "[info] **** Warning: Don't forget to chown Files to the User... ***"
+
+if [ ! "$(getent passwd ${PGID})" ]
+then
+addgroup --gid "$PGID" "mmtnrw"
+GROUP="mmtnrw"
+else
+GROUP=$(getent group ${PGID}|cut -d: -f1)
+fi
+
+if [ ! "$(getent passwd ${PUID})" ]
+then
+adduser --gecos "" --ingroup "mmtnrw" --system --uid "$PUID" "$GROUP"
+fi
+
+RUN="s6-applyuidgid -u ${PUID} -g ${PGID}"
+UMASK_SET=${UMASK_SET:-022}
+umask "$UMASK_SET"
+
+echo "[info] Setting up Timezone : $TZ"
+ln -snf /usr/share/zoneinfo/$TZ /etc/localtime
+echo $TZ > /etc/timezone
+
+echo "[info] Syncing Time...."
+ntpd -d -q -n -p time.cloudflare.com &> /dev/null
+
 echo "Starting Freeswitch Daemon"
-/usr/bin/freeswitch -nf
+chown -R $PUID:$PGID /data
+$RUN /usr/bin/freeswitch -nf
